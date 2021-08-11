c0a9c5250387a5ce45cb07e74d97e3514f9ed9f8
Kririna <kiraonnea@gmail.com>
2021-08-11 14:11:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 1_725;
+        int bonus_mile = 20;
+        int total_mile = ticket_price/bonus_mile;
+        System.out.println(total_mile);
+    }
+}
